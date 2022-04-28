3ff590c1e2eca996a2df2ddbabb803357955ef4e
Гость <Guest@iMac-Mac005-10.local>
2022-04-28 12:45:11 +0900

    kolya

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Гость on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var button5: UIButton!
+    
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBOutlet weak var button7: UIButton!
+    
+    @IBOutlet weak var button8: UIButton!
+    
+    @IBOutlet weak var button9: UIButton!
+    
+    @IBOutlet weak var button10: UIButton!
+    
+    @IBOutlet weak var button11: UIButton!
+    
+    @IBOutlet weak var button12: UIButton!
+    
+    @IBOutlet weak var button13: UIButton!
+    
+    @IBOutlet weak var button14: UIButton!
+    
+    @IBOutlet weak var button15: UIButton!
+    
+    @IBOutlet weak var button16: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
